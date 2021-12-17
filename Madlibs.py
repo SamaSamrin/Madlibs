27cefff9c10b64a4c7f59cdb15f4b018f3291645
SamaSamrin <samasamrin@gmail.com>
2021-12-17 10:39:59 +0600

    added files

---
+++ Madlibs.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 17 08:58:42 2021
+
+@author: ASUS
+"""
+
+profession = input("Enter a profession: ")
+age = input("Enter a number: ")
+adjective = input("Enter an adjective: ")
+verb = input("Enter a verb: ")
+feeling = input("Enter a feeling: ")
+
+print("Once upon a time, there was a", profession, "who was only", age, "years old.")
+print("Even though she was", adjective, ", she still liked to", verb,".")
+print("Everyone feels", feeling, "around her.")
